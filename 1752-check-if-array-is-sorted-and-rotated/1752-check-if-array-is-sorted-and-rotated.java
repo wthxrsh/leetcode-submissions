dbe4b279954c7639399644417743f609e15a1724
Harsh <63588817+wthxrsh@users.noreply.github.com>
2026-08-29 19:17:31 +0530

    Time: 0 ms (100%), Space: 42.9 MB (65.14%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int breaks = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                breaks++;
+            }
+        }
+
+        return breaks <= 1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
