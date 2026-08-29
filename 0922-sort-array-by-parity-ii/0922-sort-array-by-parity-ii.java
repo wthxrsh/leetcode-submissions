7be69c3b76593c639f56d07753ab710763acda3a
Harsh <63588817+wthxrsh@users.noreply.github.com>
2026-08-29 22:43:49 +0530

    Time: 2 ms (99.85%), Space: 48.3 MB (89.44%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
@@ -0,0 +1,27 @@
+
+class Solution {
+    public int[] sortArrayByParityII(int[] A) {
+        int i = 0, j = 1, n = A.length;
+        while (i < n && j < n) {
+            while (i < n && A[i] % 2 == 0) {
+                i += 2;
+            }
+            while (j < n && A[j] % 2 == 1) {
+                j += 2;
+            }
+            if (i < n && j < n) {
+                swap(A, i, j);
+            }
+        }
+        return A;
+    }
+    private void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
