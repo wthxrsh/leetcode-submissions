154fe09ecdd014cf7eddbb46293a7ecf946dcf1a
Harsh <63588817+wthxrsh@users.noreply.github.com>
2026-08-31 22:23:52 +0530

    Time: 5 ms (13.45%), Space: 267.4 MB (21.44%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void reverse(int[] nums, int i, int j){
+        while(i<j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
