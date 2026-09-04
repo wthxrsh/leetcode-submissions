a7c5ae38725ff3271220e07467aa01137b41754f
Harsh <63588817+wthxrsh@users.noreply.github.com>
2026-09-04 21:51:16 +0530

    Time: 1 ms (99.94%), Space: 77.2 MB (66.39%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxi = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            maxi = Math.max(maxi, sum);
+            if(sum<0) sum = 0;
+            
+        }
+        return maxi;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
