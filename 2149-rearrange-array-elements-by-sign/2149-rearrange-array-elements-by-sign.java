79489c3790b2f0e6eb9866b6334880f087b44b25
Harsh <63588817+wthxrsh@users.noreply.github.com>
2026-09-06 10:25:10 +0530

    Time: 3 ms (100%), Space: 175.7 MB (15.78%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int pos = 0;
+        int neg = 1;
+        int res[] = new int[nums.length];
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] > 0){
+                res[pos] = nums[i];
+                pos+=2;
+            }else{
+                res[neg] = nums[i];
+                neg+=2;
+            }
+        }
+        return res;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
